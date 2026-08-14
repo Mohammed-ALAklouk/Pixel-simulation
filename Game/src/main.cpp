cb853124efed4357a10202c3971d698d4fd46e12
Mohammed-ALAklouk <moha.alaklouk@gmail.com>
2026-08-15 02:43:01 +0300

    Added a crash log

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -1,7 +1,25 @@
 #include "Game.h"
 
+#include <exception>
+#include <fstream>
+
 int main()
 {
-	scree::Game app;
-	app.run();
+	try
+	{
+		scree::Game app;
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::ofstream(std::string(GetApplicationDirectory()) + "scree_crash.log")
+			<< "Unhandled exception: " << e.what() << '\n';
+		return 1;
+	}
+	catch (...)
+	{
+		std::ofstream(std::string(GetApplicationDirectory()) + "scree_crash.log")
+			<< "Unhandled non-standard exception.\n";
+		return 1;
+	}
 }
